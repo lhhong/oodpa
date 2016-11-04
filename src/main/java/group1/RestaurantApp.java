60c50a3503d7baa4d10d53c73640c9a496c37e7d
AngYang <yang_amethyst@hotmail.com>
2016-11-04 13:45:30 +0800

    Merge branch 'master' of https://github.com/lhhong/oodpa
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml

--- src/main/java/group1/RestaurantApp.java
+++ src/main/java/group1/RestaurantApp.java
@@ -40,7 +40,26 @@ public class RestaurantApp {
 			choice = userinput.nextInt();
 			switch(choice){
 				case 1:
-					menu.addItem();
+					print("Please select one of the following options:");
+					print("(1) Create Menu item");
+					print("(2) Update Menu item");
+					print("(3) Remove Menu item");
+					int input = userinput.nextInt();
+					do {
+						switch(input){
+							case 1:
+								menu.addItem();
+								break;
+							case 2:
+								menu.updateItem();
+								break;
+							case 3:
+								menu.removeItem();
+								break;
+
+						}
+					} while(input<4);
+
 					break;
 				case 2:
 					//remove promotion

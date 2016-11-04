37b5cc037d006f9e167ab687f6640f488f829c14
signapoop <leowyaoyang@gmail.com>
2016-11-04 12:40:55 +0800

    hi

--- src/main/java/group1/RestaurantApp.java
+++ src/main/java/group1/RestaurantApp.java
@@ -3,10 +3,11 @@ package group1;
 /**
  * Created by low on 4/11/16 12:08 PM.
  */
+// Hello this is Ang Yang
 public class RestaurantApp {
 	public static void main(String[] args) {
 		System.out.println("HI");
 		// HI ANGYANG
-		// HI ANGYANG 2
+
 	}
 }

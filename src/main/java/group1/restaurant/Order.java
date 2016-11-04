2cc99afdcdf6d02fc01851ad04e3c6d58e3de241
signapoop <leowyaoyang@gmail.com>
2016-11-04 13:28:29 +0800

    Added Order and Staff

--- src/main/java/group1/restaurant/Order.java
+++ src/main/java/group1/restaurant/Order.java
@@ -3,10 +3,13 @@ package group1.restaurant;
 /**
  * Created by signapoop on 4/11/16.
  */
+import group1.menu.FoodItem;
+
 import java.util.ArrayList;
 
 public class Order {
     private Staff staff;
+    private ArrayList<FoodItem> food_order;
     private int tablenumber;
 
     Order(Staff s, int t){
@@ -22,6 +25,27 @@ public class Order {
         this.staff = staff;
     }
 
+    public ArrayList<FoodItem> getFood_order() {
+        return food_order;
+    }
+
+    public void setFood_order(ArrayList<FoodItem> food_order) {
+        this.food_order = food_order;
+    }
+
+    public void printOrder(){
+        for (FoodItem f:food_order){
+            f.getDetails();
+        }
+    }
+
+    public void addItem(FoodItem f){
+        food_order.add(f);
+    }
+
+    public void removeItem(int choice){
+        food_order.remove(choice);
+    }
 
 }
 

13a246128c0d85a9a8ddaded849fe76ae57dcbb2
lhhong <heok.hong.low@gmail.com>
2016-11-04 13:10:07 +0800

    add test

--- src/test/java/group1/test/DatabaseTest.java
+++ src/test/java/group1/test/DatabaseTest.java
@@ -1,12 +1,14 @@
 package group1.test;
 
 import group1.commons.Database;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.testng.annotations.Test;
 
 /**
  * Created by low on 4/11/16 12:55 PM.
  */
 public class DatabaseTest {
+	@Ignore
 	@Test
 	public void upload() {
 		Database.save();

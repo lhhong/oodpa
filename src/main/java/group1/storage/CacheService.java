909e20b96c95c2f782ea0936119de22565d292f9
lhhong <heok.hong.low@gmail.com>
2016-11-05 15:51:43 +0800

    refined database operations

--- src/main/java/group1/storage/CacheService.java
+++ src/main/java/group1/storage/CacheService.java
@@ -1,21 +1,33 @@
 package group1.storage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by low on 4/11/16 9:56 PM.
+ * Initialize a singleton instance of cache either by retrieving from database or from mock data initialization
  */
 public class CacheService {
 
 	private static Cache cache;
+	private static final Logger logger = LoggerFactory.getLogger(CacheService.class);
 
+	/**
+	 * get singleton instance of cache
+	 * @return cache containing restaurant data
+	 */
 	public static Cache getCache() {
 		if (cache == null) {
-			Object o = Database.read();
-			if (o != null) {
-				cache = (Cache) o;
+			logger.info("initializing cache");
+			cache = Database.read();
+			if (cache == null) {
+				cache = new Cache();
+				logger.info("Cache initialized from Mock Data");
 			}
 			else {
-				cache = new Cache();
+				logger.info("Cache initialized from ftp server");
 			}
+
 		}
 		return cache;
 	}

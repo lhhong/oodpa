b9e0b8576afbca96e73bbca0b367ccfc6bf29828
lhhong <heok.hong.low@gmail.com>
2016-11-05 18:05:23 +0800

    Money class now works

--- src/main/java/group1/commons/Money.java
+++ src/main/java/group1/commons/Money.java
@@ -1,22 +1,29 @@
 package group1.commons;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by low on 4/11/16 1:38 PM.
  */
 public class Money {
+	private static final Logger logger = LoggerFactory.getLogger(Money.class);
 	public static String toString(int price) {
 		return "$" + price/100 + "." + price%100;
 	}
 	public static int parseString(String price) throws MoneyFormatException {
 		int priceInt;
 		if (price.contains(".")) {
-			String[] mixed = price.split(".");
+			String[] mixed = price.split("\\.");
+			logger.debug(Integer.toString(mixed.length));
 			if (mixed.length != 2) {
+				logger.warn("money not in 2 parts");
 				throw new MoneyFormatException();
 			}
 			try {
 				priceInt = Integer.parseInt(mixed[0]) * 100 + Integer.parseInt(mixed[1]);
 			} catch (NumberFormatException e) {
+				logger.warn("cant parse to int");
 				throw new MoneyFormatException();
 			}
 		}
@@ -24,6 +31,7 @@ public class Money {
 			try {
 				priceInt = Integer.parseInt(price) * 100;
 			} catch (NumberFormatException e) {
+				logger.warn("cant parse to int");
 				throw new MoneyFormatException();
 			}
 
--- src/test/java/group1/test/DatabaseTest.java
+++ src/test/java/group1/test/DatabaseTest.java
@@ -1,5 +1,7 @@
 package group1.test;
 
+import group1.commons.Money;
+import group1.commons.MoneyFormatException;
 import group1.storage.Cache;
 import group1.storage.Database;
 import org.junit.Ignore;
@@ -10,6 +12,20 @@ import org.testng.Assert;
  * Created by low on 4/11/16 12:55 PM.
  */
 public class DatabaseTest {
+
+	@Ignore
+	@Test
+	public void testMoney() {
+		try {
+			Assert.assertEquals(Money.parseString("12.34"), 1234);
+			Assert.assertEquals(Money.parseString("12"), 1200);
+		} catch (MoneyFormatException e) {
+			e.printStackTrace();
+		}
+		float money = (float) 14.233;
+		Assert.assertEquals(Money.parseFloat(money), 1423);
+	}
+
 	@Ignore
 	@Test
 	public void upload() {

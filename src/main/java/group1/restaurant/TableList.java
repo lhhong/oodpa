a00e4d427735151d2dae1f788fe793ec0b276405
lhhong <heok.hong.low@gmail.com>
2016-11-08 17:15:22 +0800

    added table list

--- src/main/java/group1/restaurant/TableList.java
+++ src/main/java/group1/restaurant/TableList.java
@@ -10,6 +10,25 @@ public class TableList implements Serializable {
 	private ArrayList<Table> tables;
 	private int numEmptyTables;
 
+	public TableList() {
+		int tablesize = 0;
+		int tableSizes[] = {2,4,8,10};
+		int tableDistribution[] = {10,10,5,5};
+
+        tables = new ArrayList<>(30);
+
+		int id = 0;
+        // create correct number of tables
+        for (int i = 0; i < tableDistribution.length; i++){
+            tablesize = tableSizes[i];
+
+            for (int j = 0; j < tableDistribution[i]; j++){
+                tables.add(new Table(tablesize, id++));
+            }
+        }
+        numEmptyTables = 30;
+	}
+
 	public void assignTable(/*some arguements*/) {
 		//tables.get(TableFactory.assignTable(tables, //other arguements needed)).occupy;
 		//numEmptyTables++;
--- src/main/java/group1/storage/Cache.java
+++ src/main/java/group1/storage/Cache.java
@@ -4,6 +4,7 @@ import group1.menu.Menu;
 import group1.reservation.ReservationList;
 import group1.restaurant.Staff;
 import group1.restaurant.Table;
+import group1.restaurant.TableList;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -13,29 +14,14 @@ public class Cache implements Serializable{
 
 	private LocalDate currentDay;
 
-	private ArrayList<Table> tables;
+	private TableList tables;
+	//private ArrayList<Table> tables;
 	int numEmptyTables;
 	private ArrayList<Staff> staffs;
 	private ReservationList reservations;
 	private Menu menu;
 
 	Cache() {
-		int tablesize = 0;
-		int tableSizes[] = {2,4,8,10};
-		int tableDistribution[] = {10,10,5,5};
-
-        tables = new ArrayList<>(30);
-
-		int id = 0;
-        // create correct number of tables
-        for (int i = 0; i < tableDistribution.length; i++){
-            tablesize = tableSizes[i];
-
-            for (int j = 0; j < tableDistribution[i]; j++){
-                tables.add(new Table(tablesize, id++));
-            }
-        }
-        numEmptyTables = 30;
 		currentDay = LocalDate.now();
 		staffs = MockData.getStaffs();
 		reservations = new ReservationList();

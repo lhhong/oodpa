ea944c99f174f6337f0c8173ddcb5ef217bbf89a
lhhong <heok.hong.low@gmail.com>
2016-11-08 17:13:06 +0800

    added table list

---
+++ src/main/java/group1/restaurant/TableList.java
@@ -0,0 +1,17 @@
+package group1.restaurant;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by low on 8/11/16 5:08 PM.
+ */
+public class TableList implements Serializable {
+	private ArrayList<Table> tables;
+	private int numEmptyTables;
+
+	public void assignTable(/*some arguements*/) {
+		//tables.get(TableFactory.assignTable(tables, //other arguements needed));
+		//numEmptyTables++;
+	}
+}

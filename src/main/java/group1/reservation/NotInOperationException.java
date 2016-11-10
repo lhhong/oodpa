1be95bf265bb0bc4993b3355747b30d8cc5bf48f
Thursdaylam <jordanlam009@hotmail.com>
2016-11-10 16:59:42 +0800

    Merge branch 'master' of https://github.com/lhhong/oodpa
    # Conflicts:
    #	src/main/java/group1/restaurant/TableFactory.java

--- src/main/java/group1/reservation/NotInOperationException.java
+++ src/main/java/group1/reservation/NotInOperationException.java
@@ -1,14 +1,14 @@
 package group1.reservation;
 
 /**
- * Exception when restaurant is not in operation i.e. time not within 9am to 5 pm
+ * Exception when restaurant is not in operation i.e. time not within 11am-3pm or 6pm to 10 pm
  * @author OOP Group 1
  * @version 1.0
  * @since 2016-11-8
  */
 public class NotInOperationException extends Exception{
     NotInOperationException(){
-        super("Only open from 9am to 5pm");
+        super("Only open from 11am-3pm or 6pm to 10 pm");
     }
 
 }
